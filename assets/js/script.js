408e9773e2b9d8f4c1415e19a35cf38b0fc9ac84
hammadcse <hammadrahman162@gmail.com>
2024-07-06 22:53:13 +0600

    unique room section design

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+$('.room_slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    centerMode: true,
+    arrows: true,
+    dots: false,
+    speed: 300,
+    centerPadding: '20px',
+    // infinite: false,
+    loop:false,
+    autoplaySpeed: 5000,
+    autoplay: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
