53669e438f39ed815d275d6e26a98b8622dcd15b
hammadcse <hammadrahman162@gmail.com>
2024-07-08 04:23:02 +0600

    Footer design

--- assets/js/script.js
+++ assets/js/script.js
@@ -54,3 +54,20 @@ $('.testimonial_slider').slick({
       }
     ]
   });
+
+  // Button Hover JS
+  $(function() {
+    $('.default-btn')
+    .on('mouseenter', function(e) {
+        var parentOffset = $(this).offset(),
+        relX = e.pageX - parentOffset.left,
+        relY = e.pageY - parentOffset.top;
+        $(this).find('span').css({top:relY, left:relX})
+    })
+    .on('mouseout', function(e) {
+        var parentOffset = $(this).offset(),
+        relX = e.pageX - parentOffset.left,
+        relY = e.pageY - parentOffset.top;
+        $(this).find('span').css({top:relY, left:relX})
+    });
+});
